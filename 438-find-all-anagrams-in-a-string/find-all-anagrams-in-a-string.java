bdef9c02b9c5da347a4890545625e3341d968a0c
vishalvijayakumar2306 <vishalvijayakumar2306@gmail.com>
2026-07-21 23:50:56 +0530

    Time: 63 ms (20.98%) | Memory: 48.2 MB (6.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,45 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-
-        int n = s.length();
-        int k = p.length();
-
-        if (k > n) {
-            return res;
+        List<Integer>list=new ArrayList<>();
+        int n=s.length();
+        int k=p.length();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:p.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-
-        int[] pCount = new int[26];
-        int[] wCount = new int[26];
-
-        // frequency of p
-        for (char c : p.toCharArray()) {
-            pCount[c - 'a']++;
-        }
-
-        // first window
-        for (int i = 0; i < k; i++) {
-            wCount[s.charAt(i) - 'a']++;
-        }
-
-        if (Arrays.equals(pCount, wCount)) {
-            res.add(0);
-        }
-
-        // slide window
-        for (int i = k; i < n; i++) {
-
-            // add new character
-            wCount[s.charAt(i) - 'a']++;
-
-            // remove old character
-            wCount[s.charAt(i - k) - 'a']--;
-
-            if (Arrays.equals(pCount, wCount)) {
-                res.add(i - k + 1);
+        HashMap<Character,Integer> map2=new HashMap<>();
+        int left=0;
+        for(int r=0;r<n;r++){
+            map2.put(s.charAt(r),map2.getOrDefault(s.charAt(r),0)+1);
+            if(r-left+1>k){
+                char l=s.charAt(left);
+                map2.put(l,map2.get(l)-1);
+                if (map2.get(l) == 0) {
+                    map2.remove(l);
+                }
+                left++;
+            }
+            if(r-left+1==k){
+            if(map.equals(map2)){
+                list.add(left);
             }
         }
 
-        return res;
-    }
-}
+    }return list;
+}}
